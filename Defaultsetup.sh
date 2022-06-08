834aa2580bc43a045e790adc61f977f882355139
szcarr <67342876+szcarr@users.noreply.github.com>
2022-06-08 13:50:41 +0200

    Update Defaultsetup.sh

--- Defaultsetup.sh
+++ Defaultsetup.sh
@@ -12,3 +12,8 @@ sudo apt install nmap
 sudo apt install python3-pip
 
 sudo apt install vlc
+
+mkdir ./setup_temp_default
+cd ./setup_temp_default
+wget https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64
+sudo apt install .deb
