884eb2ccf89ca8ea2083e99dee00143e5598a03c
szcarr <67342876+szcarr@users.noreply.github.com>
2022-06-08 13:38:11 +0200

    Update Defaultsetup.sh

--- Defaultsetup.sh
+++ Defaultsetup.sh
@@ -1,9 +1,14 @@
 #!/bin/bash 
 
 # wget https://raw.githubusercontent.com/szcarr/Shell/main/Defaultsetup.sh
+
 sudo apt update
 sudo apt upgrade
+
 sudo apt install ssh
+sudo apt install sshfs
 sudo apt install nmap
 
 sudo apt install python3-pip
+
+sudo apt install vlc
